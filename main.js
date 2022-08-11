f97a20f1e18a18dbffce348f5031b033d45d7d10
kendyr7 <kendyr7@gmail.com>
2022-08-10 22:26:41 -0600

    Fixing selector de categoria en JS y cambiando color de botones

--- main.js
+++ main.js
@@ -33,12 +33,12 @@ document.getElementById("formulario").addEventListener("submit",crear);
 function crear(e){
     titulo = document.getElementById("titulo").value
     descripcion = document.getElementById("descripcion").value
-    precio = document.getElementById("precio").value
+    categoria = document.getElementById("categoria").value
 
     let libro = {
         titulo,
         descripcion,
-        precio
+        categoria
     }
 
 if(localStorage.getItem("Libros") === null){
@@ -84,15 +84,15 @@ function leer(){
     for(let i=0; i<libros.length; i++){
         let titulo = libros[i].titulo
         let descripcion = libros[i].descripcion
-        let precio = libros[i].precio
+        let categoria = libros[i].categoria
 
         document.getElementById("tbody").innerHTML +=
         `<tr>
             <td>${titulo}</td>
             <td>${descripcion}</td>
-            <td>${precio}</td>
+            <td>${categoria}</td>
             <td><button onclick="eliminar('${titulo}')" class="btn btn-danger">Eliminar</button></td>
-            <td><button onclick="editar('${titulo}')" class="btn btn-success">Editar</button></td>
+            <td><button onclick="editar('${titulo}')" class="btn btn-primary">Editar</button></td>
         </tr>`
     }
 }
@@ -106,20 +106,32 @@ function editar(titulo){
             <div class="col-md-5">
                 <div class="card">
                     <div class="card-header">
-                        <h2>Editar Libro</h2>
+                        <h2>Editar Registro</h2>
                     </div>
                     <div class="card-body">
                         <form id="formulario">
                             <div class="form- mb-3">
-                                <input type="text" id="newtitulo" class="form-control" placeholder="${libros[i].titulo}">
+                                <input type="text" id="newtitulo" class="form-control" placeholder="${libros[i].titulo}" required>
                             </div>
                             <div class="form- mb-3">
-                                <textarea id="newdescripcion" class="form-control" placeholder="${libros[i].descripcion }"></textarea>
+                                <textarea id="newdescripcion" class="form-control" placeholder="${libros[i].descripcion}" required></textarea>
                             </div>
                             <div class="form- mb-3">
-                                <input type="number" id="newprecio" class="form-control" placeholder="${libros[i].precio}">
-                            </div>
-                            
+                                <select name="" id="newcategoria" class="form-control" required>
+                                    <option value="" selected="selected" disabled>Seleccione la Categoria</option>
+                                    <option value="Accion">Accion</option>
+                                    <option value="Comedia">Comedia</option>
+                                    <option value="Deportes">Deportes</option>
+                                    <option value="Aventura">Aventura</option>
+                                    <option value="Drama">Drama</option>
+                                    <option value="Fantasia">Fantasia</option>
+                                    <option value="Ciencia Ficcion">Ciencia Ficcion</option>
+                                    <option value="Crimen">Crimen</option>
+                                    <option value="Romance">Romance</option>
+                                    <option value="Misterio">Misterio</option>
+                                    <option value="Detectives">Detectives</option>
+                                </select>
+                        </div>   
                             <button class="btn btn-success" onclick="actualizar('${i}')">Actualizar</button>
                             <button class="btn btn-primary">Cancelar</button>
 
@@ -135,7 +147,7 @@ function actualizar(i){
     let libros = JSON.parse(localStorage.getItem("Libros"));
     libros[i].titulo = document.getElementById("newtitulo").value;
     libros[i].descripcion = document.getElementById("newdescripcion").value;
-    libros[i].precio = document.getElementById("newprecio").value;
+    libros[i].categoria = document.getElementById("newcategoria").value;
     localStorage.setItem("Libros",JSON.stringify(libros));
 }
 
